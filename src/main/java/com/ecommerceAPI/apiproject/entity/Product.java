3a794341a831b083784b6a9cb652247f6cb13c50
zihanlim <zihan0804@gmail.com>
2023-09-13 15:08:19 +0800

    update products api

--- src/main/java/com/ecommerceAPI/apiproject/entity/Product.java
+++ src/main/java/com/ecommerceAPI/apiproject/entity/Product.java
@@ -1,5 +1,21 @@
 package com.ecommerceAPI.apiproject.entity;
 
+import java.time.LocalDate;
+import lombok;
+
 public class Product {
-    
+    private String name;
+    private Integer productid;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getProductid() {
+        return productid;
+    }
+    public void setProductid(Integer productid) {
+        this.productid = productid;
+    }
 }
