216e0105d599b179eee81e429c60e1192fef3aab
zihanlim <zihan0804@gmail.com>
2023-09-14 12:00:07 +0800

    update products

--- src/main/java/com/ecommerceAPI/apiproject/controller/ProductController.java
+++ src/main/java/com/ecommerceAPI/apiproject/controller/ProductController.java
@@ -5,9 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+/*annotation tells the Java compiler that the class is a RESTful controller. 
+This means that the class will be responsible for handling HTTP requests and returning responses.
+*/
 @RequestMapping("/products")
+/*annotation is used in Java to map HTTP requests to handler methods in Spring MVC controllers. */
+
 public class ProductController {
     @GetMapping("")
+    /*annotation tells the Java compiler that the method is a RESTful endpoint that can be accessed using the GET HTTP method. 
+The value of the annotation is the URI of the endpoint. */
     public String getAllProducts() {
         return "all products";
     }
--- src/main/java/com/ecommerceAPI/apiproject/entity/Product.java
+++ src/main/java/com/ecommerceAPI/apiproject/entity/Product.java
@@ -1,21 +1,14 @@
 package com.ecommerceAPI.apiproject.entity;
+import lombok.Setter;
+import lombok.Getter;
 
-import java.time.LocalDate;
-import lombok;
+@Getter
+@Setter
 
 public class Product {
-    private String name;
-    private Integer productid;
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public Integer getProductid() {
-        return productid;
-    }
-    public void setProductid(Integer productid) {
-        this.productid = productid;
-    }
+
+private String productName;
+private String productCat;
+private Integer productId;
 }
+
