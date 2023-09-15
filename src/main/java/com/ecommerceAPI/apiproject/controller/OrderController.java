0488467d5bd1908a1f6f301fea091d0cd7f41444
nelson6726 <cheefong0903@gmail.com>
2023-09-15 21:26:56 +0800

    Nelson

--- src/main/java/com/ecommerceAPI/apiproject/controller/OrderController.java
+++ src/main/java/com/ecommerceAPI/apiproject/controller/OrderController.java
@@ -1,15 +1,46 @@
 package com.ecommerceAPI.apiproject.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.ecommerceAPI.apiproject.entity.Order;
+import com.ecommerceAPI.apiproject.service.OrderService;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
-    @GetMapping("")
-    public String getAllOrders() {
-        return "all orders";
+
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping("")
+    public ResponseEntity<Order> createOrder(@Valid @RequestBody Order order) {
+        return new ResponseEntity<>(orderService.createOrder(order), HttpStatus.CREATED);
     }
 
+    @GetMapping("")
+    public ResponseEntity<List<Order>> getAllOrders() {
+        return new ResponseEntity<>(orderService.getAllOrders(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> getOrderById(@PathVariable Long id) {
+        return new ResponseEntity<>(orderService.getOrderById(id), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Order> updateOrder(@PathVariable Long id, @Valid @RequestBody Order order) {
+        return new ResponseEntity<>(orderService.updateOrder(id, order), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long id) {
+        orderService.deleteOrder(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/com/ecommerceAPI/apiproject/entity/Order.java
+++ src/main/java/com/ecommerceAPI/apiproject/entity/Order.java
@@ -1,5 +1,40 @@
 package com.ecommerceAPI.apiproject.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "orders")
 public class Order {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Product name cannot be null")
+    private String productName;
+
+    @NotNull(message = "Quantity cannot be null")
+    private Integer quantity;
+
+    @NotNull(message = "Total price cannot be null")
+    private Double totalPrice;
+
+    @ManyToOne
+    private Customer customer;
 }
